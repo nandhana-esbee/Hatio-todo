e7266c97705a1bcc11403b038548b0909d3674ab
Nandhana SB <nandhanaesbee16@gmail.com>
2024-07-20 15:24:20 +0530

    added serializer file

---
+++ functions/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Todo, Project
+
+# serializer class project model
+class ProjectSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Project
+		fields = "__'all'__"
+		
+# serializer class Todo model		
+class TodoSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Todo
+		fields = ('todo_id', 'Description','Status','Updated_Date')
