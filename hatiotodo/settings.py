577ef8d60040f017c87f6fd932232c8f3c95de37
Nandhana SB <nandhanaesbee16@gmail.com>
2024-07-20 12:24:55 +0530

    Added corsheaders and django restframework

--- hatiotodo/settings.py
+++ hatiotodo/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'functions',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
