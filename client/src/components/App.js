15347106444b3a65fbeb69494e26671a595ea623
Nandhana SB <nandhanaesbee16@gmail.com>
2024-07-21 18:21:11 +0530

    added client

---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+// import components
+import Header from './Header';
+// import Projectcreate from './Projectcreate';
+// import Taskcreate from './Taskcreate';
+// import Projectcard from './Projectcard';
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <Projectcreate />
+      <Projectcard /> */}
+    </div>
+  );
+}
+
+export default App;
