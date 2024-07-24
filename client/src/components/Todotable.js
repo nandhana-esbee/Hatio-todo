a7f3e593b39532ff4f307e3af037a4bb13466fe7
Nandhana SB <nandhanaesbee16@gmail.com>
2024-07-24 12:16:29 +0530

    Todo status update ok

--- client/src/components/Todotable.js
+++ client/src/components/Todotable.js
@@ -6,12 +6,30 @@ const Todotable = (props) => {
     const date = new Date(dateString);
     const formattedDate = date.toLocaleDateString('en-IN'); 
     const formattedTime = date.toLocaleTimeString('en-IN');
-
     const [checked, setChecked] = useState(props.todoos.Status);
 
+    const ApichangeStatus = async (status) => {
+        const todo_id = props.todoos.todo_id;
+        try{
+            const response = await api.put(`/api/Todo-list/${todo_id}/`,{Status:status});
+            if(response.status === 200){
+                console.log("Status changed successfully");
+            }
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    
     const isCompleted = (e) => {
-        if(e === 'true'){setChecked('false');}
-        else{setChecked('true');}
+        if(checked === true){
+            setChecked(false);  
+            ApichangeStatus(false); 
+        }
+        else{
+        setChecked(true);
+        ApichangeStatus(true);
+        }
     }
 
     const deletetodo = async () => {
@@ -34,7 +52,7 @@ const Todotable = (props) => {
     return (
         <table className="ui blue table">
             <tr >
-            <td><input type="checkbox" value={checked} checked={checked==='true'} onChange={e=>isCompleted(e.target.value)}
+            <td><input type="checkbox" value={checked} checked={checked===true} onChange={e=>isCompleted(e.target.value)}
             style={{width:"1.5rem"}} className="left aligned checkbox"/></td>
             <th>{props.todoos.Description}</th>
             <td>{formattedDate}</td>
